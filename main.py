df82676589336cfd0e2ed7d8f769801db02c43d0
BluerZu <superluis222@hotmail.com>
2023-06-19 23:39:06 -0500

    Prototipo 2 de API

--- main.py
+++ main.py
@@ -11,12 +11,14 @@ def about():
     return{"Data":"About"}
 
 #Utilzando un slash
+#Ejemplo: 127.0.0.1:8000/invertir/123456789
 @app.get("/invertir/{cadena}")
 def invertir(cadena: str):
     cadena_invertida = cadena[::-1]
     return{"Cadena":cadena, "Cadena Invertida": cadena_invertida}
 
 #Utilizando un signo de interrogación
+#Ejemplo: 127.0.0.1:8000/invertir1?cadena=123456789
 @app.get("/invertir1")
 def invertir1(cadena: str):
     cadena_invertida = cadena[::-1]
